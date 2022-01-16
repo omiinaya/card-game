a88bc8d8e838b0c16cb108b902e878026178e5ea
Omar Minaya <omiinaya@gmail.com>
2022-01-16 06:32:46 -0500

    testing yugioh card.

--- components/card/card.jsx
+++ components/card/card.jsx
@@ -1,45 +1,35 @@
 export default function Card(props) {
-const card = { 
-    cardName: 'Baby Dragon', 
-    cardImage: 'https://i.ibb.co/wr9CV2B/img.jpg',
-    cardMonster: '[Dragon]',
-    cardDesc: 'Much more than just a child, this dragon is gifted with untapped power.',
+  const card = {
+    cardName: "Baby Dragon",
+    cardImage: "https://i.ibb.co/wr9CV2B/img.jpg",
+    cardRarity: ["x", "x", "x"],
+    cardMonster: "[Dragon]",
+    cardDesc:
+      "Much more than just a child, this dragon is gifted with untapped power.",
     cardAtk: 1200,
     cardDef: 700,
-    typeImage: 'https://i.ibb.co/nPXKRzG/wind.png'
-
-}
+    typeImage: "https://i.ibb.co/nPXKRzG/wind.png",
+  };
   return (
     <div>
       <link
         href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap"
         rel="stylesheet"
       />
-    placeholder
+      placeholder
       <div id="cardContainer">
         <img
           id="cardTemplate"
           src="https://i.ibb.co/mG6jvpR/monster-normal.png"
         />
         <img id="cardFace" src={card.cardImage} />
+        {card.cardRarity.map((card) => (
+          <img id="starLevel" key={card} src="https://i.ibb.co/M1bYC6V/star.png" />
+        ))}
         <h1 id="cardTitle">{card.cardName}</h1>
-        <img id="starLevel" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel2" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel3" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel4" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel5" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel6" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel7" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel8" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel9" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel10" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel11" src="https://i.ibb.co/M1bYC6V/star.png" />
-        <img id="starLevel12" src="https://i.ibb.co/M1bYC6V/star.png" />
         <img id="cardType" src={card.typeImage} />
         <h4 id="monsterType">{card.cardMonster}</h4>
-        <div id="monsterDescription">
-            {card.cardDesc}
-        </div>
+        <div id="monsterDescription">{card.cardDesc}</div>
         <div id="monsterAtk">{card.cardAtk}</div>
         <div id="monsterDef">{card.cardDef}</div>
       </div>
--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,16 +1,22 @@
-import Card from '../card/card'
+import Card from "../card/card";
 
 export default function Home(props) {
   const { user, origin } = props.data;
-  console.log(user)
+  console.log(user);
   return (
     <div className="container">
       <main>
-        <h1 className="title">
-          Hello world!
-        </h1>
-        <Card />
-        {user && <div>{'User ID: '}{user.id}</div>}
+        <h1 className="title">Hello world!</h1>
+        <div style={{ display: 'flex', justifyContent: 'center'}}>
+          <Card />
+          <Card />
+        </div>
+        {user && (
+          <div>
+            {"User ID: "}
+            {user.id}
+          </div>
+        )}
       </main>
     </div>
   );
