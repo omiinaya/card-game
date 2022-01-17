8a47af7efced29e42c44728c14f8550aa8775942
Omar Minaya <omiinaya@gmail.com>
2022-01-17 04:08:01 -0500

    progress on card form.

--- components/cardCreate/CardCreate.jsx
+++ components/cardCreate/CardCreate.jsx
@@ -1,38 +1,57 @@
-import { useState, useEffect } from 'react';
-import Box from '@material-ui/core/Box';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import { useState, useEffect } from "react";
+import Box from "@material-ui/core/Box";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
 
 export default function BasicSelect() {
-  const [age, setAge] = useState('Common');
+  const [name, setName] = useState("");
+  const [rarity, setRarity] = useState("Common");
 
   useEffect(() => {
-    console.log(age)
-  }, [age]);
+    console.log(rarity);
+    console.log(name);
+  }, [rarity, name]);
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleChangeRarity = (event) => {
+    setRarity(event.target.value);
   };
 
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Card Rarity: </InputLabel>
+        <InputLabel id="card-rarity">Card Rarity: </InputLabel>
         <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
+          labelId="card-rarity"
+          id="cardRarity"
+          name="cardRarity"
+          value={rarity}
           label="Card Rarity"
-          onChange={handleChange}
+          variant="standard"
+          onChange={handleChangeRarity}
         >
-          <MenuItem value={'Common'}>Common</MenuItem>
-          <MenuItem value={'Rare'}>Rare</MenuItem>
-          <MenuItem value={'Special'}>Special</MenuItem>
-          <MenuItem value={'Unique'}>Unique</MenuItem>
-          <MenuItem value={'Legendry'}>Legendary</MenuItem>
+          <MenuItem value={"Common"}>Common</MenuItem>
+          <MenuItem value={"Rare"}>Rare</MenuItem>
+          <MenuItem value={"Special"}>Special</MenuItem>
+          <MenuItem value={"Unique"}>Unique</MenuItem>
+          <MenuItem value={"Legendry"}>Legendary</MenuItem>
         </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <TextField
+          id="cardName"
+          name="cardName"
+          label="Card Name: "
+          labelId="card-name"
+          defaultValue={name}
+          onChange={handleChangeName}
+        />
       </FormControl>
     </Box>
   );
